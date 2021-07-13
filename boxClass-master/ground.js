f9c824e776bf12e7398aa71bf226368679b2b789
logicalanusha <83587369+logicalanusha@users.noreply.github.com>
2021-07-13 17:45:02 +0530

    Add files via upload

---
+++ boxClass-master/ground.js
@@ -0,0 +1,17 @@
+class Ground {
+    constructor(a,b,c,d) {
+        this.g = Bodies.rectangle(a,b,c,d,{isStatic:true});
+        World.add(world, this.g);
+        this.p = c;
+        this.q = d;
+    }
+
+    Display() {
+        rectMode(CENTER);
+        fill("blue");
+        strokeWeight(4);
+        stroke("yellow")
+        rect(this.g.position.x, this.g.position.y, this.p, this.q);
+
+    }
+}
